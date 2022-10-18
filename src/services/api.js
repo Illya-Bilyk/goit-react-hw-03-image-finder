f6a1bb6b1f0ddce557880e79f6dcefc2a128d663
Illya-Bilyk <bilikillya.i@gmail.com>
2022-10-18 12:44:58 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,13 +4,11 @@ const APIkey = '30462047-0f8848268f4f2ac11df1ed33d';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-// https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
-
 export const getImages = async (query, page) => {
-  const params = `&page=${page}&key=${APIkey}&image_type=photo&orientation=horizontal&per_page=12`;
+  const params = `&page=${page}&image_type=photo&orientation=horizontal&per_page=12`;
 
   try {
-    const response = await axios.get(`?q=${query}${params}`);
+    const response = await axios.get(`?q=${query}&key=${APIkey}${params}`);
     return response.data;
   } catch (error) {
     console.log(error);
